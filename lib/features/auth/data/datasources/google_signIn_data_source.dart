96f5ca89b2fdea4a8948653f6f258d6fd010fd11
forswow <f0rsw0w@yandex.ru>
2025-06-30 22:51:54 +0400

    fix(auth): восстановление жестко закодированных clientId и serverClientId в GoogleSignInDataSource

--- lib/features/auth/data/datasources/google_signIn_data_source.dart
+++ lib/features/auth/data/datasources/google_signIn_data_source.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/foundation.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 const List<String> _scopes = <String>[
@@ -24,8 +23,9 @@ class GoogleSignInDataSourceImpl implements GoogleSignInDataSource {
   Future<void> _ensureInitialized() async {
     if (_initialized) return;
     await _signIn.initialize(
-      clientId: dotenv.get('GoogleClientID'),
-      serverClientId: dotenv.get('GoogleClientSecret'),
+      clientId:
+          '',
+      serverClientId: '',
     );
     // Можно подписаться на события, если нужно
     // _googleSignIn.authenticationEvents.listen(...);
