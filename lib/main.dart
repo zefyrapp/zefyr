f7aea413dcca9da07082a909f7950108014e26d2
forswow <f0rsw0w@yandex.ru>
2025-06-30 21:55:16 +0400

    feat(auth): integrate dotenv for Google sign-in client ID and secret management

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:zefyr/core/app/app.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await dotenv.load();
   runApp(const ProviderScope(child: MyApp()));
 }
